b919c620aab3130cde51e0ab01de98c7d843c6d4
Bui Sy Tu <tubs@tandan.com.vn>
2022-02-08 08:28:00 +0700

    update

--- src/Infrastructure/FileStorage/Services/CommonService.cs
+++ src/Infrastructure/FileStorage/Services/CommonService.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TD.OpenData.WebApi.Infrastructure.FileStorage.Services;
+﻿namespace TD.OpenData.WebApi.Infrastructure.FileStorage.Services;
 
 public static class CommonService
 {
     public static async Task<Stream?> ReadFromUrl(string? url)
     {
-        if (url is null) return null;
+        if (string.IsNullOrEmpty(url)) return null;
 
         using HttpClient client = new();
         byte[]? content = await client.GetByteArrayAsync(url);
